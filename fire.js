aea4a3274d21646cc1244541871da8b9e9f28142
Samuel Nascimento <nascimentto.samuel@gmail.com>
2020-06-13 05:12:47 -0300

    swicth tables for canvas, add fire options and style refactoring

--- fire.js
+++ fire.js
@@ -1,83 +1,131 @@
+const canvas = document.getElementById('fire-canvas')
+const increaseDiv = document.getElementById('increase-option')
+const decreaseDiv = document.getElementById('decrease-option')
+const destroyDiv = document.getElementById('destroy-option')
+const maxFireDiv = document.getElementById('max-option')
+const fireColorsPalette = [{ "r":7,"g":7,"b":7 }, { "r":31,"g":7,"b":7 }, { "r":47,"g":15,"b":7 }, { "r":71,"g":15,"b":7 }, { "r":87,"g":23,"b":7 }, { "r":103,"g":31,"b":7 }, { "r":119,"g":31,"b":7 }, { "r":143,"g":39,"b":7 }, { "r":159,"g":47,"b":7 }, { "r":175,"g":63,"b":7 }, { "r":191,"g":71,"b":7 },{ "r":199,"g":71,"b":7 }, { "r":223,"g":79,"b":7 }, { "r":223,"g":87,"b":7 },{ "r":223,"g":87,"b":7 }, { "r":215,"g":95,"b":7 }, { "r":215,"g":95,"b":7 }, { "r":215,"g":103,"b":15 }, { "r":207,"g":111,"b":15 }, { "r":207,"g":119,"b":15 }, { "r":207,"g":127,"b":15 }, { "r":207,"g":135,"b":23 }, { "r":199,"g":135,"b":23 }, { "r":199,"g":143,"b":23 }, { "r":199,"g":151,"b":31 }, { "r":191,"g":159,"b":31 }, { "r":191,"g":159,"b":31 }, { "r":191,"g":167,"b":39 }, { "r":191,"g":167,"b":39 }, { "r":191,"g":175,"b":47 }, { "r":183,"g":175,"b":47 }, { "r":183,"g":183,"b":47 }, { "r":183,"g":183,"b":55 }, { "r":207,"g":207,"b":111 }, { "r":223,"g":223,"b":159 }, { "r":239,"g":239,"b":199 }, { "r":255,"g":255,"b":255 }]
 const firePixelsArray = []
-const fireWidth = 50
-const fireHeigth = 50
-const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
+const fireWidth = 60
+const fireHeight = 40
+canvas.width = fireWidth
+canvas.height = fireHeight
+const ctx = canvas.getContext('2d')
+const image = ctx.createImageData(fireWidth, fireHeight)
 
-function start(){
-    createFireDataStructure()
-    createFireSource()
-    renderFire()
+// Listeners
+increaseDiv.addEventListener('click', () => increaseFireSource())
+decreaseDiv.addEventListener('click', () => decreaseFireSource())
+destroyDiv.addEventListener('click', () => destroyFireSource())
+maxFireDiv.addEventListener('click', () => maxFireSource())
 
-    setInterval(calculeFirePropagation, 50)
+function start () {
+  createFireDataStructure()
+  createFireSource()
+  renderFire()
+
+  setInterval(calculeFirePropagation, 50)
 }
 
-function createFireDataStructure(){
-    const numberOfPixel = fireWidth * fireHeigth
-    for(let i = 0; i < numberOfPixel; i++){
-        firePixelsArray[i]=0        
+function createFireDataStructure () {
+  const numberOfPixel = fireWidth * fireHeight
+  for (let i = 0; i < numberOfPixel; i++) {
+    firePixelsArray[i] = 0
+  }
+}
+
+function calculeFirePropagation () {
+  for (let column = 0; column < fireWidth; column++) {
+    for (let row = 0; row < fireHeight; row++) {
+      const pixelIndex = column + (fireWidth * row)
+      updateFireIntensityPerPixel(pixelIndex)
     }
+  }
+  renderFire()
 }
 
-function calculeFirePropagation(){ 
-    for (let column = 0; column < fireWidth; column++){
-        for(let row = 0; row < fireHeigth; row++){
-            const pixelIndex = column + (fireWidth * row )
-            updateFireIntensityPerPixel(pixelIndex)
-        }
+function updateFireIntensityPerPixel (currentPixelIndex) {
+  const bellowPixelIndex = currentPixelIndex + fireWidth
+
+  if (bellowPixelIndex >= fireWidth * fireHeight) {
+    return
+  }
+
+  const decay = Math.floor(Math.random() * 3)
+  const bellowPixelFireIntensity = firePixelsArray[bellowPixelIndex]
+  const newFireIntensity = bellowPixelFireIntensity - decay >= 0 ? bellowPixelFireIntensity - decay : 0
+
+  firePixelsArray[currentPixelIndex - decay] = newFireIntensity
+}
+
+function renderFire () {
+  for (let pixelIndex = 0; pixelIndex < firePixelsArray.length; pixelIndex++) {
+    const fireIntensity = firePixelsArray[pixelIndex]
+    const color = fireColorsPalette[fireIntensity]
+
+    image.data[pixelIndex * 4] = color.r
+    image.data[pixelIndex * 4 + 1] = color.g
+    image.data[pixelIndex * 4 + 2] = color.b
+    image.data[pixelIndex * 4 + 3] = 255
+  }
+
+  ctx.putImageData(image, 0, 0)
+}
+
+function createFireSource () {
+  for (let column = 0; column <= fireWidth; column++) {
+    const overflowPixelIndex = fireWidth * fireHeight
+    const pixelIndex = (overflowPixelIndex - fireWidth) + column
+
+    firePixelsArray[pixelIndex] = 36
+  }
+}
+
+function destroyFireSource () {
+  for (let column = 0; column <= fireWidth; column++) {
+    const overflowPixelIndex = fireWidth * fireHeight
+    const pixelIndex = (overflowPixelIndex - fireWidth) + column
+    firePixelsArray[pixelIndex] = 0
+  }
+}
+
+function maxFireSource () {
+  for (let column = 0; column <= fireWidth; column++) {
+    const overflowPixelIndex = fireWidth * fireHeight
+    const pixelIndex = (overflowPixelIndex - fireWidth) + column
+    firePixelsArray[pixelIndex] = 36
+  }
+}
+
+function increaseFireSource () {
+  for (let column = 0; column <= fireWidth; column++) {
+    const overflowPixelIndex = fireWidth * fireHeight
+    const pixelIndex = (overflowPixelIndex - fireWidth) + column
+    const currentFireIntensity = firePixelsArray[pixelIndex]
+
+    if (currentFireIntensity < 36) {
+      const increase = Math.floor(Math.random() * 14)
+      const newFireIntensity =
+        currentFireIntensity + increase >= 36 ? 36 : currentFireIntensity + increase
+
+      firePixelsArray[pixelIndex] = newFireIntensity
     }
-
-    renderFire()
+  }
 }
 
-function updateFireIntensityPerPixel(currentPixelIndex){
-    const bellowPixelIndex = currentPixelIndex + fireWidth
+function decreaseFireSource () {
+  for (let column = 0; column <= fireWidth; column++) {
+    const overflowPixelIndex = fireWidth * fireHeight
+    const pixelIndex = (overflowPixelIndex - fireWidth) + column
+    const currentFireIntensity = firePixelsArray[pixelIndex]
 
-    if(bellowPixelIndex>=fireWidth * fireHeigth){
-        return
+    if (currentFireIntensity > 0) {
+      const decay = Math.floor(Math.random() * 14)
+      const newFireIntensity =
+        currentFireIntensity - decay >= 0 ? currentFireIntensity - decay : 0
+
+      firePixelsArray[pixelIndex] = newFireIntensity
     }
-    
-    const decay = Math.floor(Math.random()* 3)
-    const bellowPixelFireIntensity = firePixelsArray[bellowPixelIndex]
-    const newFireIntensity= bellowPixelFireIntensity - decay >= 0 ? bellowPixelFireIntensity - decay : 0
-
-    firePixelsArray[currentPixelIndex - decay] = newFireIntensity
+  }
 }
 
-function renderFire(){
-    const debug = false
-    let html = '<table cellpadding=0 cellspacing=0>'
-    for (let row=0; row< fireHeigth; row++){
-        html += '<tr>'
-        
-        for (let column=0; column<fireWidth; column++){
-            const pixelIndex = column + (fireWidth * row)
-            const fireIntensity = firePixelsArray[pixelIndex] 
-
-            if(debug == true){
-                html += '<td>'
-                html += `<div class="pixel-index">${pixelIndex}</div>`
-                html += fireIntensity
-                html += '</td>'
-            }else{
-                const color = fireColorsPalette[fireIntensity]
-                const colorString = `${color.r},${color.g}, ${color.b}`
-                html += `<td class="pixel" style ="background-color: rgb(${colorString})">` 
-            }
-
-        }
-    
-        html += '</tr>'
-    }
-    html+='</table>'
-    document.querySelector('#fireCanvas').innerHTML = html
-}
-
-function createFireSource(){
-    for( let column = 0; column <=fireWidth; column++){
-        const overflowPixelIndex = fireWidth * fireHeigth
-        const pixelIndex = (overflowPixelIndex - fireWidth) + column
-
-        firePixelsArray[pixelIndex] = 36
-    }
-}
 start()
